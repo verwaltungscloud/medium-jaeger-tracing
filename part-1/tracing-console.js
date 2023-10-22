c62cdeb4a314df105c94c07ea9a9e895c7c67e8a
lumaghg <robin.reyer@t-online.de>
2023-10-22 22:33:49 +0200

    add next parts

--- part-1/tracing-console.js
+++ part-1/tracing-console.js
@@ -1,33 +1,42 @@
-//OpenTelemetry
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
-//instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
-const { JaegerPropagator } = require("@opentelemetry/propagator-jaeger");
 
+/**
+ * This file contains the tracing configuration for printing to the console
+ */
 
 //Exporter
 module.exports = (serviceName) => {
-    const exporter = new JaegerExporter({ endpoint: "http://jaeger:14268/api/traces" });
+
+    //Register a new Trace Provider in the global tracer registry under the service name
     const provider = new NodeTracerProvider({
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
     });
+    //Configure Span Processor and Exporter 
+    const exporter = new ConsoleSpanExporter();
     provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-    provider.register({ propagator: new JaegerPropagator() });
+
+    //Configure Instrumentations
     registerInstrumentations({
         instrumentations: [
+            //creates spans for http requests
             new HttpInstrumentation(),
-            new ExpressInstrumentation()
+            //creates spans for http request from the express perspective
+            //expects the http instrumentation to be registered
+            new ExpressInstrumentation(),
         ],
+        //pass in the provider so the instrumentations can create spans
         tracerProvider: provider,
     });
+    //return the tracer
     return trace.getTracer(serviceName);
 };
--- part-1/tracing.js
+++ part-1/tracing.js
@@ -1,39 +1,36 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { JaegerPropagator } = require("@opentelemetry/propagator-jaeger");
+
+
+/**
+ * This file contains the tracing configuration for exporting the traces to jaeger
+ */
 
 
 //Exporter
 module.exports = (serviceName) => {
-
-    //Register a new Trace Provider in the global tracer registry under the service name
+    const exporter = new JaegerExporter({ endpoint: "http://jaeger:14268/api/traces" });
     const provider = new NodeTracerProvider({
         resource: new Resource({
             [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
         }),
     });
-    //Configure Span Processor and Exporter 
-    const exporter = new ConsoleSpanExporter();
     provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
-    //Configure Instrumentations
+    provider.register({ propagator: new JaegerPropagator() });
     registerInstrumentations({
         instrumentations: [
-            //creates spans for http requests
             new HttpInstrumentation(),
-            //creates spans for http request from the express perspective
-            //expects the http instrumentation to be registered
-            new ExpressInstrumentation(),
+            new ExpressInstrumentation()
         ],
-        //pass in the provider so the instrumentations can create spans
         tracerProvider: provider,
     });
-    //return the tracer
     return trace.getTracer(serviceName);
 };
